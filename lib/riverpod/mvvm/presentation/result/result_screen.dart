20b6bb17876a5c1f21429107a352ad771d34ead0
SHT-3756 <gkdlfndkekd@naver.com>
2023-12-20 16:04:49 +0900

    feat : 스크린 생성 (my,point,select,screen)

---
+++ lib/riverpod/mvvm/presentation/result/result_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_state_compare/riverpod/mvvm/domian/riverpod/app_provider.dart';
+
+class ResultScreen extends ConsumerWidget {
+  const ResultScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final appState = ref.watch(appStateProvider);
+    return Container(
+      color: Colors.yellow,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Center(
+            child: appState.isLogin ? const Text('로그인') : Text('비로그인이니 돌아가'),
+          )
+        ],
+      ),
+    );
+  }
+}
