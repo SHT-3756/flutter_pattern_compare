7a2ff3a9ff356f65a8d85c958858afd595eb215e
SHT-3756 <gkdlfndkekd@naver.com>
2023-12-20 16:08:48 +0900

    feat : 코드 추가

--- lib/bloc/mvvm/presentation/result/result_screen.dart
+++ lib/bloc/mvvm/presentation/result/result_screen.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_state_compare/riverpod/mvvm/domian/riverpod/app_provider.dart';
 
-class ResultScreen extends StatelessWidget {
+class ResultScreen extends ConsumerWidget {
   const ResultScreen({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
+  Widget build(BuildContext context, WidgetRef ref) {
+    final appState = ref.watch(appStateProvider);
+    return Container(
+      color: Colors.purple,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Center(
+            child: appState.isLogin ? const Text('로그인') : Text('비로그인이니 돌아가'),
+          ),
+        ],
+      ),
+    );
   }
 }
